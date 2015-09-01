bd2ced9c66e30884c2c6e7746048bc8c6454c93f
raqsilva <raqsilva8@hotmail.com>
2015-09-01 10:26:25 +0100

    javascript code to static file

---
+++ static/css/tool_js.js
@@ -0,0 +1,96 @@
+
+function showonlyone(thechosenone) {
+      var newboxes = document.getElementsByTagName("div");
+            for(var x=0; x<newboxes.length; x++) {
+                  name = newboxes[x].getAttribute("class");
+                  if (name == 'newboxes') {
+                        if (newboxes[x].id == thechosenone) {
+                        newboxes[x].style.display = 'block';
+                  }
+                  else {
+                        newboxes[x].style.display = 'none';
+                  }
+            }
+      }
+}
+
+
+
+function showonlyone2(thechosenone) {
+      var boxes = document.getElementsByTagName("div");
+            for(var x=0; x<boxes.length; x++) {
+                  name = boxes[x].getAttribute("class");
+                  if (name == 'boxes') {
+                        if (boxes[x].id == thechosenone) {
+                        boxes[x].style.display = 'block';
+                  }
+                  else {
+                        boxes[x].style.display = 'none';
+                  }
+            }
+      }
+}
+
+
+
+function SelectHandler(select){
+		if(select.value == 'xlsx'){
+				$("#select_xlsx").show()
+				$("#select_vcf").hide();
+								}
+		else if(select.value == 'vcf'){
+				$("#select_vcf").show();
+				$("#select_xlsx").hide();
+				}
+		}
+
+
+
+$(document).ready(function() {
+    $('#id_populations_5_0').click(function(event) {  //on click
+        if(this.checked) { // check select status
+            $('.checkbox1').each(function() { //loop through each checkbox
+                this.checked = true;  //select all checkboxes with class "checkbox1"              
+            });
+        }else{
+            $('.checkbox1').each(function() { //loop through each checkbox
+                this.checked = false; //deselect all checkboxes with class "checkbox1"                      
+            });        
+        }
+    });
+   
+});
+
+
+
+$(document).ready(function() {
+    $('#id_columns_12').click(function(event) {  //on click
+        if(this.checked) { // check select status
+            $('.column1').each(function() { 
+                this.checked = true;      
+            });
+        }else{
+            $('.column1').each(function() { 
+                this.checked = false;                      
+            });        
+        }
+    });
+   
+});
+
+
+
+$(document).ready(function() {
+    $('#id_exac_col_44').click(function(event) {  //on click
+        if(this.checked) { // check select status
+            $('.add_all').each(function() { 
+                this.checked = true;         
+            });
+        }else{
+            $('.add_all').each(function() { 
+                this.checked = false;             
+            });        
+        }
+    });
+   
+});
