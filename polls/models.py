1e80c7ddf616a1b49024296262550c01004b749d
raqsilva <raqsilva8@hotmail.com>
2016-06-15 21:49:50 +0100

    reformat models

--- polls/models.py
+++ polls/models.py
@@ -5,26 +5,27 @@ from django.contrib.auth.models import User
 import datetime
 
 
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     activation_key = models.CharField(max_length=40)
     key_expires = models.DateTimeField()
-  
-    
+
+
 class Document(models.Model):
-    docfile = models.FileField(upload_to = 'documents', blank = True)
+    docfile = models.FileField(upload_to='documents', blank=True)
     user_profile = models.ForeignKey(UserProfile)
 
-@receiver(pre_delete, sender=Document)
+
+@receiver(pre_delete, sender = Document)
 def document_delete(sender, instance, **kwargs):
     instance.docfile.delete(False)
 
 
 class Vcf(models.Model):
-    vcf_file = models.FileField(upload_to = 'documents', blank = True)
+    vcf_file = models.FileField(upload_to='documents', blank=True)
     user_profile = models.ForeignKey(UserProfile)
 
-@receiver(pre_delete, sender=Vcf)
+
+@receiver(pre_delete, sender = Vcf)
 def vcf_delete(sender, instance, **kwargs):
     instance.vcf_file.delete(False)
